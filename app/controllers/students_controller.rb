162c8db51e615aa3ba49a630a5bc3efa9b42b7f8
Shun Yao <ysmiracle@hotmail.com>
2018-04-29 17:18:07 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,8 +13,8 @@ class StudentsController < ApplicationController
   # end
 
   def create
-    binding.pry
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to students_path
+    binding.pry
+    redirect_to student_path
   end
 end
